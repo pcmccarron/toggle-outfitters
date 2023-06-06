54f196d1f2343e0338c077033765c53e4cb12a4d
Peter McCarron <pcmccarron@gmail.com>
2023-06-06 11:01:52 -0600

    temp update of flag trigger

--- src/hooks/useErrorHandling.ts
+++ src/hooks/useErrorHandling.ts
@@ -1,18 +1,28 @@
 // hooks/useErrorHandling.ts
-
-/******************************************************************************
- * If you're going to try the flag trigger code, you need to replace everything
- * below with what is in the guide.
- *******************************************************************************/
-
-
 import { useState } from 'react';
 import { useShoppingCart } from 'use-shopping-cart';
 
 const useErrorHandling = () => {
   const [errorState, setErrorState] = useState(false);
-  
+
   const { clearCart } = useShoppingCart();
+
+  const Trigger = async () => {
+    try {
+      const response = await fetch(
+        // put your flag trigger URL in here using this format: 'http://your-flag-trigger.com', (needs both the comma and quotes)   
+        {
+          method: "POST",
+          body: JSON.stringify({
+            eventName: "There was an error with the API",
+          }),
+        }
+      );
+      return response.status;
+    } catch (error) {
+      console.log("the fetch did not work");
+    }
+  };
 
   const errorTesting = async () => {
     try {
@@ -24,6 +34,7 @@ const useErrorHandling = () => {
       if (jsonData == "the API is unreachable") {
         setErrorState(true);
          clearCart()
+         Trigger()
         return 502;
       } else {
         setErrorState(false);
